70f1b3120839810c9850073cd91a8279fd74c08f
Surya142002 <surya142002@gmail.com>
2023-03-04 12:56:14 -0500

    start guessing game

--- NumberGuessingGame.py
+++ NumberGuessingGame.py
@@ -1,1 +1,12 @@
+'''
+Objective is to guess a random number between 1 and 100
+
+There are 5 attempts
+
+After each attempt, the user will know if their answer is either
+- Correct
+- Too High
+- Too Low
+- Incorrect: Out of Guesses!
+'''
 
