fbad87f570d538d34aa4d8761820c4a8ca4fad54
Surya142002 <surya142002@gmail.com>
2023-03-04 13:26:48 -0500

    finished guessing game

--- NumberGuessingGame.py
+++ NumberGuessingGame.py
@@ -10,3 +10,28 @@ After each attempt, the user will know if their answer is either
 - Incorrect: Out of Guesses!
 '''
 
+import random
+
+target = random.randint(1, 100)
+winner = False
+
+print("You have 5 guesses to find the random number between 1 and 100!")
+
+for i in range(1,6):
+    guess = input("What is your " + str(i) + " guess? ")
+    if int(guess) > target:
+        print("Too High!")
+    elif int(guess) < target:
+        print("Too Low!")
+    else:
+        print("Correct!")
+        winner = True
+        break
+
+print("The number was: " + str(target))
+
+if winner:
+    print("Good Guess You Win!!")
+else:
+    print("You Are Out Of Guesses! Better Luck Next Time!")
+
