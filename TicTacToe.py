23edf6dc63c21f587400c79c584d853a1f014af2
Surya142002 <surya142002@gmail.com>
2023-03-09 09:25:54 -0500

    finished tic tac toe

--- TicTacToe.py
+++ TicTacToe.py
@@ -8,9 +8,28 @@ mark is either
 - O
 - X
 '''
+def checkWinner(options):
+    # check vertical
+    for i in range(0,4):
+        if options[i] != " " and options[i] == options[i + 3] and options[i + 3] == options[i + 6]:
+            print("GAME OVER PLAYER " + options[i] + " WINS!!")
+            return True
 
+    # check horizontal
+    for i in range(0,7,3):
+        if options[i] != " " and options[i] == options[i + 1] and options[i + 1] == options[i + 2]:
+            print("GAME OVER PLAYER " + options[i] + " WINS!!")
+            return True
 
-def createBoard():
+    # check 2 diagonals
+    if options[1] != " " and options[1] == options[5] and options[5] == options[9]:
+        print("GAME OVER PLAYER " + options[1] + " WINS!!")
+        return True
+    if options[3] != " " and options[3] == options[5] and options[5] == options[7]:
+        print("GAME OVER PLAYER " + options[3] + " WINS!!")
+        return True
+
+def createBoard(options):
     print("***** Board Layout *****")
     print(" 1 | 2 | 3 ")
     print("--- --- ---")
@@ -19,19 +38,36 @@ def createBoard():
     print(" 7 | 8 | 9 ")
     print()
     print("***** Current Board *****")
-    # TODO: Update Board
-
+    print(" " + options[0] + " | " + options[1] + " | " + options[2] + " ")
+    print("--- --- ---")
+    print(" " + options[3] + " | " + options[4] + " | " + options[5] + " ")
+    print("--- --- ---")
+    print(" " + options[6] + " | " + options[7] + " | " + options[8] + " ")
+    print("************************")
 
 gameOver = False
 player1 = True
-options = ["", "", "",
-           "", "", "",
-           "", "", ""]
+options = [" ", " ", " ",
+           " ", " ", " ",
+           " ", " ", " "]
 
+createBoard(options)
 while not gameOver:
     if player1:
         choice = input("Please enter Choice Player 1 (You are X): ")
+        if options[int(choice) - 1] != " ":
+            print("Invalid Choice Try Again!")
+            continue
+        else:
+            options[int(choice) - 1] = "X"
         player1 = False
     else:
         choice = input("Please enter Choice Player 2 (You are O): ")
+        if options[int(choice) - 1] != " ":
+            print("Invalid Choice Try Again!")
+            continue
+        else:
+            options[int(choice) - 1] = "O"
         player1 = True
+    createBoard(options)
+    gameOver = checkWinner(options)
